1343a5d26026c890f781d5c4d2a4bd766f55a46e
TheJat <manishkl543@gmail.com>
2026-05-23 22:30:55 +0530

    Request-Context: refactor

--- core/http/request-context.ts
+++ core/http/request-context.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 
 export class RequestContext {
-  constructor(
-    public req: Request,
-    public res: Response,
-    public plugin?: any,
-    public user?: any
-  ) {}
+    constructor(
+        public req: Request,
+        public res: Response,
+        public plugin?: any,
+        public user?: any
+    ) { }
 
+
+    get body() {
+        return this.req.body;
+    }
+
+    get params() {
+        return this.req.params;
+    }
+
+    get query() {
+        return this.req.query;
+    }
 }
